f70d8257e1fac8be303effaab73d9cf8a95f8dba
jcesarmobile <jcesarmobile@gmail.com>
2024-12-12 19:52:34 +0100

    WIP: Migrate plugins to Capacitor 7

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,34 @@ import { rimraf } from 'rimraf';
 import { logger } from './log';
 import { runCommand } from './subprocess';
 
-const coreVersion = '^6.0.0';
-const gradleVersion = '8.2.1';
-const AGPVersion = '8.2.1';
-const gmsVersion = '4.4.0';
-const kotlinVersion = '1.9.10';
+const coreVersion = 'next';
+const gradleVersion = '8.11.1';
+const AGPVersion = '8.7.2';
+const gmsVersion = '4.4.2';
+const kotlinVersion = '1.9.25';
 const eslintVersion = '^8.57.0';
 const ionicEslintVersion = '^0.4.0';
+const ionicSwiftlintVersion = '^2.0.0';
+const rimrafVersion = '^6.0.1';
 const variables = {
-  minSdkVersion: 22,
-  compileSdkVersion: 34,
-  targetSdkVersion: 34,
-  androidxActivityVersion: '1.8.0',
-  androidxAppCompatVersion: '1.6.1',
+  minSdkVersion: 23,
+  compileSdkVersion: 35,
+  targetSdkVersion: 35,
+  androidxActivityVersion: '1.9.2',
+  androidxAppCompatVersion: '1.7.0',
   androidxCoordinatorLayoutVersion: '1.2.0',
-  androidxCoreVersion: '1.12.0',
-  androidxFragmentVersion: '1.6.2',
-  firebaseMessagingVersion: '23.3.1',
-  playServicesLocationVersion: '21.1.0',
-  androidxBrowserVersion: '1.7.0',
-  androidxMaterialVersion: '1.10.0',
-  androidxExifInterfaceVersion: '1.3.6',
-  androidxCoreKTXVersion: '1.12.0',
-  googleMapsPlayServicesVersion: '18.2.0',
-  googleMapsUtilsVersion: '3.8.2',
-  googleMapsKtxVersion: '5.0.0',
-  googleMapsUtilsKtxVersion: '5.0.0',
-  kotlinxCoroutinesVersion: '1.7.3',
+  androidxCoreVersion: '1.15.0',
+  androidxFragmentVersion: '1.8.4',
+  firebaseMessagingVersion: '24.1.0',
+  playServicesLocationVersion: '21.3.0',
+  androidxBrowserVersion: '1.8.0',
+  androidxMaterialVersion: '1.12.0',
+  androidxExifInterfaceVersion: '1.3.7',
   coreSplashScreenVersion: '1.0.1',
-  androidxWebkitVersion: '1.9.0',
+  androidxWebkitVersion: '1.12.1',
   junitVersion: '4.13.2',
-  androidxJunitVersion: '1.1.5',
-  androidxEspressoCoreVersion: '3.5.1',
+  androidxJunitVersion: '1.2.1',
+  androidxEspressoCoreVersion: '3.6.1',
 };
 
 process.on('unhandledRejection', (error) => {
@@ -68,9 +64,18 @@ export const run = async (): Promise<void> => {
       pluginJSON.devDependencies['eslint'] = eslintVersion;
     }
   }
+  if (pluginJSON.devDependencies?.['@ionic/swiftlint-config']) {
+    pluginJSON.devDependencies['@ionic/swiftlint-config'] = ionicSwiftlintVersion;
+  }
+  if (pluginJSON.devDependencies?.['swiftlint']) {
+    pluginJSON.devDependencies['swiftlint'] = ionicSwiftlintVersion;
+  }
+  if (pluginJSON.devDependencies?.['rimraf']) {
+    pluginJSON.devDependencies['rimraf'] = rimrafVersion;
+  }
 
-  if (pluginJSON.version.startsWith('5.')) {
-    pluginJSON.version = '6.0.0';
+  if (pluginJSON.version.startsWith('6.')) {
+    pluginJSON.version = '7.0.0';
   }
 
   await writeJSON(packageJson, pluginJSON, { spaces: 2 });
@@ -92,18 +97,12 @@ export const run = async (): Promise<void> => {
     if (await pathExists(androidDir)) {
       logger.info('Updating Android files');
 
-      updateGradleProperties(join(androidDir, 'gradle.properties'));
       const variablesAndClasspaths = {
         variables: variables,
         'com.android.tools.build:gradle': AGPVersion,
         'com.google.gms:google-services': gmsVersion,
       };
       await updateBuildGradle(join(androidDir, 'build.gradle'), variablesAndClasspaths);
-
-      await movePackageFromManifestToBuildGradle(
-        join(androidDir, 'src', 'main', 'AndroidManifest.xml'),
-        join(androidDir, 'build.gradle'),
-      );
 
       updateGradleWrapper(join(androidDir, 'gradle', 'wrapper', 'gradle-wrapper.properties'));
 
@@ -119,7 +118,7 @@ export const run = async (): Promise<void> => {
     }
   }
 
-  logger.info('Plugin migrated to Capacitor 6!');
+  logger.info('Plugin migrated to Capacitor 7!');
 };
 
 async function updateBuildGradle(
@@ -136,8 +135,8 @@ async function updateBuildGradle(
   }
   gradleFile = gradleFile.replaceAll(' =  ', ' = ');
   logger.info('Updating build.gradle');
-  gradleFile = setAllStringIn(gradleFile, `sourceCompatibility JavaVersion.`, `\n`, `VERSION_17`);
-  gradleFile = setAllStringIn(gradleFile, `targetCompatibility JavaVersion.`, `\n`, `VERSION_17`);
+  gradleFile = setAllStringIn(gradleFile, `sourceCompatibility JavaVersion.`, `\n`, `VERSION_21`);
+  gradleFile = setAllStringIn(gradleFile, `targetCompatibility JavaVersion.`, `\n`, `VERSION_21`);
 
   const neededDeps: { [key: string]: string } = {
     'com.android.tools.build:gradle': variablesAndClasspaths['com.android.tools.build:gradle'],
@@ -192,12 +191,6 @@ async function updateBuildGradle(
     `:$kotlin_version`,
   );
 
-  const compileSdk = `compileSdkVersion project.hasProperty`;
-  if (gradleFile.includes(compileSdk)) {
-    logger.info('Replacing deprecated compileSdkVersion from build.gradle');
-    gradleFile = gradleFile.replace(compileSdk, `compileSdk project.hasProperty`);
-  }
-
   writeFileSync(filename, gradleFile, 'utf-8');
 }
 
@@ -245,76 +238,4 @@ function setAllStringIn(data: string, start: string, end: string, replacement: s
     }
   }
   return result;
-}
-
-async function movePackageFromManifestToBuildGradle(manifestFilename: string, buildGradleFilename: string) {
-  const manifestText = readFile(manifestFilename);
-  const buildGradleText = readFile(buildGradleFilename);
-
-  if (!manifestText) {
-    logger.error(`Could not read ${manifestFilename}. Check its permissions and if it exists.`);
-    return;
-  }
-
-  if (!buildGradleText) {
-    logger.error(`Could not read ${buildGradleFilename}. Check its permissions and if it exists.`);
-    return;
-  }
-  const namespaceExists = new RegExp(/\s+namespace\s+/).test(buildGradleText);
-  if (namespaceExists) {
-    return;
-  }
-  let packageName: string;
-  const manifestRegEx = new RegExp(/package="([^"]+)"/);
-  const manifestResults = manifestRegEx.exec(manifestText);
-
-  if (manifestResults === null) {
-    logger.error(`Unable to update Android Manifest. Package not found.`);
-    return;
-  } else {
-    packageName = manifestResults[1];
-  }
-
-  let manifestReplaced = manifestText;
-
-  manifestReplaced = manifestReplaced.replace(manifestRegEx, '');
-  manifestReplaced = manifestReplaced.replace(` \n`, '\n');
-
-  if (manifestText == manifestReplaced) {
-    logger.error(`Unable to update Android Manifest: no changes were detected in Android Manifest file`);
-    return;
-  }
-
-  let buildGradleReplaced = buildGradleText;
-  buildGradleReplaced = setAllStringIn(buildGradleText, 'android {', '\n', `\n    namespace "${packageName}"`);
-
-  if (buildGradleText == buildGradleReplaced) {
-    logger.error(`Unable to update buildGradleText: no changes were detected in Android Manifest file`);
-    return;
-  }
-
-  logger.info('Moving package from AndroidManifest.xml to build.gradle');
-
-  writeFileSync(manifestFilename, manifestReplaced, 'utf-8');
-  writeFileSync(buildGradleFilename, buildGradleReplaced, 'utf-8');
-}
-
-async function updateGradleProperties(filename: string) {
-  const txt = readFile(filename);
-  if (!txt?.includes('android.enableJetifier=true')) {
-    return;
-  }
-  logger.info('Remove android.enableJetifier=true from gradle.properties');
-  const lines = txt.split('\n');
-  let linesToKeep = '';
-  for (const line of lines) {
-    // check for enableJetifier
-    const jetifierMatch = line.match(/android\.enableJetifier\s*=\s*true/) || [];
-    const commentMatch = line.match(/# Automatically convert third-party libraries to use AndroidX/) || [];
-
-    if (jetifierMatch.length == 0 && commentMatch.length == 0) {
-      linesToKeep += line + '\n';
-    }
-  }
-  writeFileSync(filename, linesToKeep, { encoding: 'utf-8' });
 }
