e60e5fbd1c3965123398645981f86ae79772f97f
Rui Mendes <rui.moreira.mendes@outsystems.com>
2025-12-02 16:10:44 +0000

    remove types/node check

--- src/index.ts
+++ src/index.ts
@@ -103,9 +103,7 @@ export const run = async (): Promise<void> => {
   }
   if (pluginJSON.devDependencies?.['typescript']) {
     pluginJSON.devDependencies['typescript'] = typeScriptVersion;
-    if (pluginJSON.devDependencies?.['@types/node']) {
-      pluginJSON.devDependencies['@types/node'] = typesNodeVersion;
-    }
+    pluginJSON.devDependencies['@types/node'] = typesNodeVersion;
   }
 
   let prettierUpdatedFromV2 = false;
