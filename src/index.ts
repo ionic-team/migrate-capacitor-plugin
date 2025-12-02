187033b817ef0bff71d938cb4a264657d76e2585
Rui Mendes <rui.moreira.mendes@outsystems.com>
2025-12-02 14:36:22 +0000

    added new regex to update kotlin_version

--- src/index.ts
+++ src/index.ts
@@ -308,6 +308,14 @@ async function updateBuildGradle(
     return `ext.kotlin_version = project.hasProperty("kotlin_version") ? rootProject.ext.kotlin_version : '${kotlinVersion}'`;
   });
 
+  const blockkotlinRegex = /ext\s*\{[\s\S]*?\}/gm;
+  gradleFile = gradleFile.replace(blockkotlinRegex, (block) => {
+    return block.replace(
+      /kotlin_version\s*=\s*['"][^'"]+['"]|kotlin_version\s*=\s*project\.hasProperty\("kotlin_version"\)\s*\?\s*rootProject\.ext\.kotlin_version\s*:\s*['"][^'"]+['"]/,
+      `kotlin_version = project.hasProperty("kotlin_version") ? rootProject.ext.kotlin_version : '${kotlinVersion}'`,
+    );
+  });
+
   gradleFile = setAllStringIn(
     gradleFile,
     `implementation "org.jetbrains.kotlin:kotlin-stdlib`,
